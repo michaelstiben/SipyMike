296a93f649598e6bdbcf9ce2ce9a39cbcc8c329f
Michael Paredes <michaelstiben@gmail.com>
2019-06-28 00:35:59 -0500

    added some libs, but reporting a crash bug

--- main.py
+++ main.py
@@ -3,11 +3,4 @@ import time
 
 pycom.heartbeat(False)
 
-for cycles in range(3): # stop after 10 cycles
-    pycom.rgbled(0x007f00) # green
-    s.send('red')
-    time.sleep(5)
-    pycom.rgbled(0x7f7f00) # yellow
-    time.sleep(1.5)
-    pycom.rgbled(0x7f0000) # red
-    time.sleep(4)
+pycom.rgbled(0x111111)
